3781d877ae061a9e3f4e39be28bfc35bfe99d2d9
c-a-ray <c.a.ray@outlook.com>
2021-11-15 18:02:07 -0800

    Add comments

--- line_processor.c
+++ line_processor.c
@@ -1,3 +1,19 @@
+/**
+ * @file line_processor.c
+ * @author Cody Ray <rayc2@oregonstate.edu>
+ * @version 1.0
+ * @section DESCRIPTION
+ *
+ * For OSU CS 344
+ * Assignment 4
+ * 
+ * This program creates four threads to process input from stdin using the Producer-Consumer model:
+ *  Thread 1 (input thread): reads lines of characters from stdin
+ *  Thread 2 (line separator thread): replaces every line separator with a space
+ *  Thread 3 (plus sign thread): replaces every pair of plus signs with a caret
+ *  Thread 4 (output thread): writes processed data to stdout as 80-character lines
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -5,16 +21,20 @@
 #include <stdbool.h>
 #include "line_processor.h"
 
-bool stop_processing = false;
+bool stop_processing = false; // Whether the stop processing line has occurred
 
-char buffer_1[MAX_INPUT_LINE * MAX_LINES];
-pthread_mutex_t buf_1_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t buf_1_full = PTHREAD_COND_INITIALIZER;
-bool buf_1_closed = false;
-int buf_1_count = 0;
-int buf_1_prod_idx = 0;
-int buf_1_cons_idx = 0;
+// Buffer 1 variables
+// Input thread produces; line separator thread consumes
+char buffer_1[MAX_INPUT_LINE * MAX_LINES];                  // Buffers are large enough for 50 lines of 1000 characters so buffers can be unbounded
+pthread_mutex_t buf_1_mutex = PTHREAD_MUTEX_INITIALIZER;    // To indicate when a critical section of code needs exclusive access
+pthread_cond_t buf_1_full = PTHREAD_COND_INITIALIZER;       // Signal to indicate to consumer that the buffer is empty
+bool buf_1_closed = false;                                  // Whether buffer is closed or not
+int buf_1_count = 0;                                        // Number of characters in buffer
+int buf_1_prod_idx = 0;                                     // Location producer inserts a character to
+int buf_1_cons_idx = 0;                                     // Location consumer reads a character from
 
+// Buffer 2 variables
+// Line separator thread produces; plus sign thread consumes
 char buffer_2[MAX_INPUT_LINE * MAX_LINES];
 pthread_mutex_t buf_2_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t buf_2_full = PTHREAD_COND_INITIALIZER;
@@ -23,6 +43,8 @@ int buf_2_count = 0;
 int buf_2_prod_idx = 0;
 int buf_2_cons_idx = 0;
 
+// Buffer 3 variables
+// Plus sign thread produces; output thread consumes
 char buffer_3[MAX_INPUT_LINE * MAX_LINES];
 pthread_mutex_t buf_3_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t buf_3_full = PTHREAD_COND_INITIALIZER;
@@ -32,13 +54,18 @@ int buf_3_cons_idx = 0;
 
 int main(void)
 {
-    pthread_t input_t, line_separate_t, plus_replace_t, output_t;
+    pthread_t input_t;          // Input thread
+    pthread_t line_separate_t;  // Line separator thread
+    pthread_t plus_replace_t;   // Plus sign thread
+    pthread_t output_t;         // Output thread
 
+    // Create threads
     pthread_create(&input_t, NULL, get_input, NULL);
     pthread_create(&line_separate_t, NULL, separate_line, NULL);
     pthread_create(&plus_replace_t, NULL, replace_double_plus, NULL);
     pthread_create(&output_t, NULL, write_output, NULL);
 
+    // Close threads
     pthread_join(input_t, NULL);
     pthread_join(line_separate_t, NULL);
     pthread_join(plus_replace_t, NULL);
@@ -49,21 +76,16 @@ int main(void)
 
 void *get_input(void *args)
 {
-    for (int i = 0; i < MAX_LINES; i++)
+    for (int i = 0; i < MAX_LINES; i++)                 // At most, 50 lines will be read
     {
-        char input_line[MAX_INPUT_LINE];
-        fgets(input_line, MAX_INPUT_LINE, stdin);
+        char input_line[MAX_INPUT_LINE];                // Allocate enough space for a 1,000 character line
+        fgets(input_line, MAX_INPUT_LINE, stdin);       // Read a line from stdin
     
-        for (int i = 0; i < strlen(input_line); i++)
-        {            
-            if (is_stop_line(input_line))
-            {
-                stop_processing = true;
-                pthread_cond_signal(&buf_1_full);
-                return NULL;
-            }
-            put_buf_1(input_line[i]);
-        }
+        if (is_stop_line(input_line))                   // Check if the line is the stop processing line
+            return stop_input();                        // Stop processing line found, so stop reading input
+
+        for (int i = 0; i < strlen(input_line); i++)    // Walk through each character in the input line
+            put_buf_1(input_line[i]);                   // Write the character to buffer one
     }
     return NULL;
 }
@@ -71,165 +93,173 @@ void *get_input(void *args)
 
 void *separate_line(void *args)
 {
+    // Process at most 50 lines of 1,000 characters
     char ch;
     for (int i = 0; i < MAX_INPUT_LINE * MAX_LINES; i++)
     {
-        ch = read_buf_1();
-
-        if (ch == 0)
-            break;
-        else if (ch == NEWLINE)
-            put_buf_2(SPACE);
+        ch = read_buf_1();          // Read the next character from buffer 1
+        if (ch == 0)                // ch is 0 if buffer 1 is empty and the stop processing line has been given
+            break;                  // Stop processing characters
+        else if (ch == NEWLINE)     // Check if the character is the line separator
+            put_buf_2(SPACE);       // Write a space to buffer 2 instead of the line separator
         else
-            put_buf_2(ch);
+            put_buf_2(ch);          // Write the character to buffer 2
     }
     return NULL;
 }
 
 void *replace_double_plus(void *args)
 {
+    // Process at most 50 lines of 1,000 characters
     char ch;
     for (int i = 0; i < MAX_INPUT_LINE * MAX_LINES; i++)
     {
-        ch = read_buf_2();
-
-        if (ch == 0)
-            break;
-        else if (ch == PLUS_SIGN && next_char_is_plus())
+        ch = read_buf_2();                                  // Read the next character from buffer 2
+        if (ch == 0)                                        // ch is 0 if buffer 2 is empty and buffer 1 is closed
+            break;                                          // Stop processing characters
+        else if (ch == PLUS_SIGN && next_char_is_plus())    // Check if the current character and the next character are both plus signs
         {
-            read_buf_2();
-            put_buf_3(CARET);
+            read_buf_2();                                   // Skip the next plus sign in buffer 2
+            put_buf_3(CARET);                               // Write a caret to buffer 3 instead of the plus signs
         }
         else
-            put_buf_3(ch);
+            put_buf_3(ch);                                  // Write the character to buffer 3
     }
     return NULL;
 }
 
 void *write_output(void *args)
 {
+    // Allocate enough space for 80 characters + a line separator
     char output_line[OUTPUT_LINE_LEN + 1];
     char ch;
 
+    // Process at most 50 lines of 1,000 characters
     for (int i = 0; i < MAX_INPUT_LINE * MAX_LINES; i++)
     {
-        ch = read_buf_3();
-
-        if (ch == 0)
-            break;
-        else if (i < OUTPUT_LINE_LEN)
-        {
-            output_line[i] = ch;
-            if (i == OUTPUT_LINE_LEN - 1)
-                printf("%s\n", output_line);
-        }
+        ch = read_buf_3();                          // Read the next character from buffer 3
+        if (ch == 0)                                // ch is 0 if buffer 3 is empty and buffer 2 is closed
+            break;                                  // Stop processing characters
         else
-        {
-            output_line[i % OUTPUT_LINE_LEN] = ch;
-            if ((i + 1) % OUTPUT_LINE_LEN == 0)
-                printf("%s\n", output_line);
+        {                                           
+            output_line[i % OUTPUT_LINE_LEN] = ch;  // Copy the character to the output line
+            if ((i + 1) % OUTPUT_LINE_LEN == 0)     // Check if there are enough characters for an output line
+                printf("%s\n", output_line);        // Write the output line to stdout
         }
     }
     return NULL;
 }
 
-void put_buf_1(char item)
+void put_buf_1(char ch)
 {
-  pthread_mutex_lock(&buf_1_mutex);
-  buffer_1[buf_1_prod_idx++] = item;
-  buf_1_count++;
-  pthread_cond_signal(&buf_1_full);
-  pthread_mutex_unlock(&buf_1_mutex);
+  pthread_mutex_lock(&buf_1_mutex);     // Lock the mutex to ensure exclusive access
+  buffer_1[buf_1_prod_idx++] = ch;      // Write the character to buffer one and increment the producer index
+  buf_1_count++;                        // Increment the number of characters in buffer one
+  pthread_cond_signal(&buf_1_full);     // Signal thread two that buffer one is not empty
+  pthread_mutex_unlock(&buf_1_mutex);   // Unlock the mutex
 }
 
 char read_buf_1(void)
 {
-    pthread_mutex_lock(&buf_1_mutex);
-    while (buf_1_count == 0)
+    pthread_mutex_lock(&buf_1_mutex);                   // Lock the mutex to ensure exclusive access
+    while (buf_1_count == 0)                            // Check whether buffer one is empty
     {
-        if (stop_processing)
+        if (stop_processing)                            // Check if the stop processing line has been given
         {
-            buf_1_closed = true;
-            pthread_cond_signal(&buf_2_full);
-            return 0;
+            buf_1_closed = true;                        // Close buffer one
+            pthread_cond_signal(&buf_2_full);           // Signal thread three
+            return 0;                                   // Tell thread two to shut down
         }
-        pthread_cond_wait(&buf_1_full, &buf_1_mutex);
+        pthread_cond_wait(&buf_1_full, &buf_1_mutex);   // Wait for the buffer one full signal to read a character
     }
-    char ch = buffer_1[buf_1_cons_idx++];
-    buf_1_count--;
-    pthread_mutex_unlock(&buf_1_mutex);
-    return ch;
+    char ch = buffer_1[buf_1_cons_idx++];               // Read the next character from buffer one
+    buf_1_count--;                                      // Decrement the number of characters in buffer one
+    pthread_mutex_unlock(&buf_1_mutex);                 // Unlock the mutex
+    return ch;                                          // Return the character read from buffer one
 }
 
 void put_buf_2(char ch)
 {
-    pthread_mutex_lock(&buf_2_mutex);
-    buffer_2[buf_2_prod_idx++] = ch;
-    buf_2_count++;
-    pthread_cond_signal(&buf_2_full);
-    pthread_mutex_unlock(&buf_2_mutex);
+    pthread_mutex_lock(&buf_2_mutex);   // Lock the mutex to ensure exclusive access
+    buffer_2[buf_2_prod_idx++] = ch;    // Write the character to buffer two and increment the producer index
+    buf_2_count++;                      // Increment the number of characters in buffer two
+    pthread_cond_signal(&buf_2_full);   // Signal thread three that buffer two is not empty
+    pthread_mutex_unlock(&buf_2_mutex); // Unlock the mutex
 }
 
 
 char read_buf_2(void)
 {
-    pthread_mutex_lock(&buf_2_mutex);
-    while (buf_2_count == 0)
+    pthread_mutex_lock(&buf_2_mutex);                   // Lock the mutex to ensure exclusive access
+    while (buf_2_count == 0)                            // Check whether buffer two is empty
     {
-        if (buf_1_closed)
+        if (buf_1_closed)                               // Check whether buffer one is closed
         {
-            buf_2_closed = true;
-            pthread_cond_signal(&buf_3_full);
-            return 0;
+            buf_2_closed = true;                        // Close buffer two
+            pthread_cond_signal(&buf_3_full);           // Signal buffer three
+            return 0;                                   // Tell buffer three to shut down
         }
-        pthread_cond_wait(&buf_2_full, &buf_2_mutex);
+        pthread_cond_wait(&buf_2_full, &buf_2_mutex);   // Wait for the buffer two full signal to read a character
     }
-    char ch = buffer_2[buf_2_cons_idx++];
-    buf_2_count--;
-    pthread_mutex_unlock(&buf_2_mutex);
-    return ch;
+    char ch = buffer_2[buf_2_cons_idx++];               // Read the next character from buffer two
+    buf_2_count--;                                      // Decrement the number of characterfs in buffer two
+    pthread_mutex_unlock(&buf_2_mutex);                 // Unlock the mutex
+    return ch;                                          // Return the character read from buffer two
 }
 
 void put_buf_3(char ch)
 {
-    pthread_mutex_lock(&buf_3_mutex);
-    buffer_3[buf_3_prod_idx++] = ch;
-    buf_3_count++;
-    pthread_cond_signal(&buf_3_full);
-    pthread_mutex_unlock(&buf_3_mutex);
+    pthread_mutex_lock(&buf_3_mutex);    // Lock the mutex to ensure exclusive access
+    buffer_3[buf_3_prod_idx++] = ch;     // Write the character to buffer three and increment the producer index
+    buf_3_count++;                       // Increment the number of characters in buffer three
+    pthread_cond_signal(&buf_3_full);    // Signal thread four that buffer three is not empty
+    pthread_mutex_unlock(&buf_3_mutex);  // Unlock the mutex
 }
 
 char read_buf_3(void)
 {
-    pthread_mutex_lock(&buf_3_mutex);
-    while (buf_3_count == 0)
+    pthread_mutex_lock(&buf_3_mutex);                   // Lock the mutex to ensure exclusive access
+    while (buf_3_count == 0)                            // Check whether buffer three is empty
     {
-        if (buf_2_closed)
-            return 0;
-        pthread_cond_wait(&buf_3_full, &buf_3_mutex);
+        if (buf_2_closed)                               // Check whether buffer two is closed
+            return 0;                                   // Tell thread four to shut down
+        pthread_cond_wait(&buf_3_full, &buf_3_mutex);   // Wait for the buffer three full signal to read a character
     } 
-    char ch = buffer_3[buf_3_cons_idx++];
-    buf_3_count--;
-    pthread_mutex_unlock(&buf_3_mutex);
-    return ch;
+    char ch = buffer_3[buf_3_cons_idx++];               // Read the next character from buffer three
+    buf_3_count--;                                      // Decrement the number of characters in buffer three
+    pthread_mutex_unlock(&buf_3_mutex);                 // Unlock the mutex
+    return ch;                                          // Return the character read from buffer three
+}
+
+void *stop_input(void)
+{
+    pthread_mutex_lock(&buf_1_mutex);    // Lock the buffer one mutex
+    stop_processing = true;              // Indicate that the stop processing line has been given
+    pthread_cond_signal(&buf_1_full);    // Signal thread two
+    pthread_mutex_unlock(&buf_1_mutex);  // Unlock the mutex
+    return NULL;                         // Tell get_input to shut down
 }
 
 bool is_stop_line(char input_line[])
 {
-    char stop_line[6] = "STOP\n";
-    if (strlen(input_line) < strlen(stop_line))
+    char stop_line[6] = "STOP\n"; // The stop line
+
+    // If the input line doesn't start with 'S' or isn't six characters long, it's not a stop line
+    if (input_line[0] != 'S' || strlen(input_line) < strlen(stop_line))
         return false;
-    for (int i = 0; i < strlen(stop_line); i++)
-        if (input_line[i] != stop_line[i])
-            return false;
-    return true;
+
+    for (int i = 0; i < strlen(stop_line); i++)     // Compare every character in the input line to every character in the stop line
+        if (input_line[i] != stop_line[i])          // If any are different
+            return false;                           // The input line is not the stop line
+
+    return true;                                    // If we get here, the input line is the stop line
 }
 
 bool next_char_is_plus(void)
 {
-    pthread_mutex_lock(&buf_2_mutex);
-    bool next_char_is_plus = buffer_2[buf_2_cons_idx] == PLUS_SIGN ? true : false;
-    pthread_mutex_unlock(&buf_2_mutex);
-    return next_char_is_plus;
+    pthread_mutex_lock(&buf_2_mutex);                                                   // Lock the buffer two mutex
+    bool next_char_is_plus = buffer_2[buf_2_cons_idx] == PLUS_SIGN ? true : false;      // Check whether the next character is a plus sign
+    pthread_mutex_unlock(&buf_2_mutex);                                                 // Unlock the mutex
+    return next_char_is_plus;                                                           // Return whether the next character is a plus sign or not
 }
 
--- line_processor.h
+++ line_processor.h
@@ -1,25 +1,148 @@
+/**
+ * @file line_processor.h
+ * @author Cody Ray <rayc2@oregonstate.edu>
+ * @version 1.0
+ * @section DESCRIPTION
+ *
+ * For OSU CS 344
+ * Assignment 4
+ * 
+ * Macros and function declarations for line_processor.c
+ */
+
 #ifndef LINE_PROCESSOR
 #define LINE_PROCESSOR
 
-#define MAX_INPUT_LINE 1000
-#define MAX_LINES 50
-#define OUTPUT_LINE_LEN 80
-#define SPACE ' '
-#define NEWLINE '\n'
-#define PLUS_SIGN '+'
-#define CARET '^'
+// Input/output sizes
+#define MAX_INPUT_LINE 1000 // Maximum length of an input line (including newline)
+#define MAX_LINES 50        // Maximum number of input lines (including STOP line)
+#define OUTPUT_LINE_LEN 80  // Fixed length for all output lines
 
+// Important characters
+#define NEWLINE '\n'        // Line separator
+#define SPACE ' '           // Any instance of a line separator is replaces with a space
+#define CARET '^'           // Caret character
+#define PLUS_SIGN '+'       // Any instance of two plus signs is replaced with a caret
+
+/**
+ * Thread one. Reads lines of characters from stdin, checks for the stop 
+ * processing line, and writes characters to buffer one.
+ * 
+ * @param  args required but not used
+ * @return required but not used
+ */
 void *get_input(void *);
+
+/**
+ * Thread two. Reads characters from buffer one, replaces all newlines
+ * with spaces, and writes characters to buffer two.
+ * 
+ * @param  args required but not used
+ * @return required but not used
+ */
 void *separate_line(void *);
+
+/**
+ * Thread three. Reads characters from buffer two, replaces all instances
+ * of two plusses ('++') with a caret ('^'), and writes characters to
+ * buffer three.
+ * 
+ * @param  args required but not used
+ * @return required but not used
+ */
 void *replace_double_plus(void *);
+
+/**
+ * Thread four. Reads characters from buffer three and writes eighty character
+ * lines to stdout when and if there are eighty characters available to write.
+ * 
+ * @param  args required but not used
+ * @return required but not used
+ */
 void *write_output(void *);
+
+/**
+ * Locks the buffer one mutex, writes the given character to buffer one, and
+ * signals thread two that buffer one is not empty.
+ * 
+ * @param  ch a character to write to buffer one
+ */
 void put_buf_1(char);
+
+/**
+ * Locks the buffer one mutex and tries to read the next character from buffer
+ * one. If buffer one is empty, waits for the buffer one full signal. If
+ * buffer one is empty and the stop processing line has been given, returns 0
+ * to indicate that thread two should shut down. Otherwise returns the next 
+ * character from buffer one.
+ * 
+ * @return the next character from buffer one. Returns 0 if buffer one is empty
+ *         and the stop processing line has been given.
+ */
 char read_buf_1(void);
+
+/**
+ * Locks the buffer two mutex, writes the given character to buffer two, and
+ * signals thread three that buffer two is not empty.
+ * 
+ * @param  ch a character to write to buffer two
+ */
 void put_buf_2(char);
+
+/**
+ * Locks the buffer two mutex and tries to read the next character from buffer
+ * two. If buffer two is empty, waits for the buffer two full signal. If
+ * buffer two is empty and buffer one is closed, returns 0 to indicate that
+ * thread two should shut down. Otherwise returns the next character
+ * from buffer two.
+ * 
+ * @return the next character from buffer two. Returns 0 if buffer two is empty
+ *         and buffer one is closed.
+ */
 char read_buf_2(void);
+
+/**
+ * Locks the buffer three mutex, writes the given character to buffer three, and
+ * signals thread four that buffer three is not empty.
+ * 
+ * @param  ch a character to write to buffer three
+ */
 void put_buf_3(char);
+
+/**
+ * Locks the buffer three mutex and tries to read the next character from buffer
+ * three. If buffer three is empty, waits for the buffer three full signal. If
+ * buffer three is empty and buffer two is closed, returns 0 to indicate that
+ * thread three should shut down. Otherwise returns the next character
+ * from buffer three.
+ * 
+ * @return the next character from buffer three. Returns 0 if buffer three is empty
+ *         and buffer two is closed.
+ */
 char read_buf_3(void);
+
+/**
+ * Locks the buffer one mutex, switches the stop_processing variable to true, and
+ * signal thread two to take over.
+ * 
+ * @return NULL to tell get_input to stop processing input
+ */
+void *stop_input(void);
+
+/**
+ * Checks if an input line is the stop processing line ('STOP\n')
+ * 
+ * @param  input_line an input line represented as a character array
+ * @return true if the input line is the stop processing line, otherwise false
+ */
 bool is_stop_line(char []);
+
+/**
+ * Locks the buffer two mutex and peeks at the next character in buffer two to see if
+ * it is a plus sign.
+ * 
+ * @return true if the next character in buffer two is a plus sign, otherwise false
+ */
 bool next_char_is_plus(void);
 
 #endif
